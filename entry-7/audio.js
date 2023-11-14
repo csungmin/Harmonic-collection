060eb0680c3b3e7fbb1ac0994946c2aded5c5bea
csungmin <chuns045@newschool.edu>
2023-11-13 23:22:48 -0500

    comments

--- entry-7/audio.js
+++ entry-7/audio.js
@@ -2,3 +2,9 @@ window.addEventListener ('click', function () {
     document.getElementById("ducksound"). play ();
     });
 
+// I got this source from youtube video
+
+// window.addEventListener ('click', function () {       => When user click, the event starts
+//     document.getElementById("ducksound"). play ();    
+// => This is the function when user clicks
+// => It will find the element ID "ducksound" from HTML and play it.
--- entry-7/cursor.js
+++ entry-7/cursor.js
@@ -1,8 +1,16 @@
-document.addEventListener("mousemove", (e) => {
+document.addEventListener("mousemove", (e) => {   
     const cursor = document.querySelector(".cursor");
     cursor.style.left = e.pageX - cursor.clientWidth / 1.5 + "px";
     cursor.style.top = e.pageY - cursor.clientHeight / 1.5 + "px";
     
 });
 
+
 // I got this code from ChatGPT as a source
+
+// document.addEventListener("mousemove", => following the mouse (e) => {  => "e" represents the event object and in this case it is inclduing the mouse coordinates
+//     const cursor = document.querySelector(".cursor");    => This is letting my HTML to know that my "cursor" that I want to follow 
+//     cursor.style.left = e.pageX - cursor.clientWidth / 1.5 + "px";
+//     cursor.style.top = e.pageY - cursor.clientHeight / 1.5 + "px";
+// => "e.pageX" is mouse coordinate horizontally, and "e.pageY" is mouse coordinate vertically, 
+// => "1.5" is dividing the cursor's width and height. This is responsive to the movement of the mouse.
