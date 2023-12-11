744d72bd620f0136b6a31c607e3a83b97a8f1938
csungmin <chuns045@newschool.edu>
2023-12-11 02:43:59 -0500

    entry1

---
+++ entry-1/script.js
@@ -0,0 +1,7 @@
+// I got this code from youtube search
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Getting the element ID using 'img-to-drag'
+    let el = document.getElementById('img-to-drag');
+    let draggie = new Draggabilly(el);
+});
